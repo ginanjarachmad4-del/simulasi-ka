4bb1eb9207c257613fc6c82ef69033a1b81afd49
ginanjarachmad4-del <ginanjarachmad4@gmail.com>
2026-06-11 17:37:12 +0700

    Implement clock update functionality
    Added a clock update function that displays the current time in WIB format.

--- app.js
+++ app.js
@@ -135,3 +135,16 @@ landing.style.display="none";
 },700);
 
 };
+
+function updateClock(){
+  const now = new Date();
+  const time = now.toLocaleTimeString("id-ID", {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+  document.getElementById("clock").innerText = time + " WIB";
+}
+
+setInterval(updateClock, 1000);
+updateClock();
